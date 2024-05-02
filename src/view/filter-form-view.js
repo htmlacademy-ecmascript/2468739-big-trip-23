32a91c12e86788e0e67aef400d9aea3ae3756e98
Roman <Melnikov_RV@list.ru>
2024-05-02 13:30:57 +0700

    Сделает отдельные функции для шаблона одного фильтра и для шаблона одной сортировки

--- src/view/filter-form-view.js
+++ src/view/filter-form-view.js
@@ -1,27 +1,17 @@
 import { createElement } from '../render.js';
 
+const FILTER_TYPES = ['everything', 'future', 'present', 'past'];
+
+const createFilterFormItemTemplate = (filterType) => `<div class="trip-filters__filter">
+<input id="filter-${filterType}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filterType}" checked>
+<label class="trip-filters__filter-label" for="filter-${filterType}">${filterType[0].toUpperCase()}${filterType.slice(
+  1
+)}</label>
+</div>`;
+
 const createFilterFormTemplate =
   () => `<form class="trip-filters" action="#" method="get">
-<div class="trip-filters__filter">
-  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-</div>
-
-<div class="trip-filters__filter">
-  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-  <label class="trip-filters__filter-label" for="filter-future">Future</label>
-</div>
-
-<div class="trip-filters__filter">
-  <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-  <label class="trip-filters__filter-label" for="filter-present">Present</label>
-</div>
-
-<div class="trip-filters__filter">
-  <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-  <label class="trip-filters__filter-label" for="filter-past">Past</label>
-</div>
-
+${FILTER_TYPES.map((type) => createFilterFormItemTemplate(type)).join('')}
 <button class="visually-hidden" type="submit">Accept filter</button>
 </form>`;
 
